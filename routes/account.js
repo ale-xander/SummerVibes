8374bee13ada6cd81c7af1f5b5a4a7d02201a71f
jlister <jsphentrep14@gmail.com>
2019-07-22 13:51:25 -0700

    added shop route

--- routes/account.js
+++ routes/account.js
@@ -4,10 +4,10 @@ const ctrl = require('../controllers');
 
 router.get('/signup', ctrl.accountsCtrl.newUser);  
 router.post('/signup', ctrl.accountsCtrl.createUser); 
-router.get('/'login', ctrl.accountsCtrl.newSession); 
-router.post('/'login', ctrl.accountsCtrl.createSession); 
-router.get('/'logout, ctrl.accountsCtrl.deleteSession);
-router.post('/'logout, ctrl.accoutnCtrl.deleteSession); 
+router.get('/login', ctrl.accountsCtrl.newSession); 
+router.post('/login', ctrl.accountsCtrl.createSession); 
+router.get('/logout', ctrl.accountsCtrl.deleteSession);
+router.post('/logout', ctrl.accoutnCtrl.deleteSession); 
 
 module.express = router; 
 
--- routes/profile.js
+++ routes/profile.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const ctrl = require('.. controllers');
- 
-router.get('/', ctrl.profileCtrl.showProfile); 
+const ctrl = require('../controllers');
+
+
+router.get('/', ctrl.profileCtrl.showProfile);
 
 module.exports = router;
+
+
+
 
 
 
