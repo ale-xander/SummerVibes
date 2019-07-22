05d61609367732530788e72d4b93d1256988fb09
alexiliev <iliev.alex@outlook.com>
2019-07-22 14:48:07 -0700

    updating the account Controllers

--- controllers/accountCtrl.js
+++ controllers/accountCtrl.js
@@ -1,11 +1,24 @@
 const db = require('../models');
 const responseFunc = require('./resposnse');
+//sendResponse, sendErrorResponse come from controllers/response.js
 
 module.exports = {
     index: (req, res) =>    {
         db.Customer.find({}, (error, foundCustomer)=>{
-            if(error) return responseFunc.sendErrorResponse(res, error);
-            responseFunc.sendErrorResponse(res, foundCustomer)
+            if(error) return sendResponse.sendErrorResponse(res, error);
+            sendResponse.sendErrorResponse(res, foundCustomer);
+        });
+    },
+    show: (req, res) =>     {
+        db.Customer.findOne({name: req.params.name}, (error, foundCustomer)=>{
+            if(error) return sendResponse.sendErrorResponse(res, error);
+            sendResponse.sendErrorResponse(res, foundCustomer);
+        });
+    },
+    delete: (req,res) =>    {
+        db.Customer.findOneAndDelete({name: req.params.name}, (error, deletedCustomer) =>{
+            if(error) return sendResponse.sendErrorResponse(res, error);
+            sendResponse.sendErrorResponse(res, foundCustomer);
         })
     }
 }
