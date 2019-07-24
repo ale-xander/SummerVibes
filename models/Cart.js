480a05e6999518848741da2a11edafdf65d03eb9
jlister <jsphentrep14@gmail.com>
2019-07-23 19:28:19 -0700

    fixed referencing bug

--- models/Cart.js
+++ models/Cart.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const Item = require('./Item');
 
 const CartSchema = new Schema({
     userId:{
         type: String,
         required: true
     },
-    items:[Items.Schema],
+    items:[{
+        type: Schema.Types.ObjectId,
+        ref: 'Item'
+      }]
 });
 
 const Cart = mongoose.model('Cart', CartSchema);
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const session = require('express-session');
+const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
