33615c99018c40716b08956ada0f06b976ea9a92
jlister <jsphentrep14@gmail.com>
2019-07-23 19:50:54 -0700

    created new working atlas link

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const DB_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/';
+const DB_URL = 'mongodb+srv://joseph:fEV5nmsWx1TPd3A2@cluster0-s2bob.mongodb.net/test?retryWrites=true&w=majority';
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
