8d8c44e7d4a133cb0842a83a7072ecf342d84118
alexiliev <iliev.alex@outlook.com>
2019-07-24 16:42:07 -0700

    Server has a profile endpoint. profile has routes for GET, PUT and POST. profileCtrl has update and delete.

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-// const ctrl = require('../controllers');
+const profileCtrl = require('../controllers/profileCtrl');
 
 
-// router.get('/', ctrl.profileCtrl.showProfile);
+router.get('/profile', profileCtrl.showProfile);
+router.put('/profile', profileCtrl.updateProfile);
+router.post('/profile', profileCtrl.deleteProfile)
 
 module.exports = router;
 
