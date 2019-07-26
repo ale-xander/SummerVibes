4ea39ec8c31a96b486c86b3e0112e74d53385e03
alexiliev <iliev.alex@outlook.com>
2019-07-26 12:12:41 -0700

    adds heroku vars

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
-const DB_URL = 'mongodb+srv://joseph:fEV5nmsWx1TPd3A2@cluster0-s2bob.mongodb.net/summer-vibes?retryWrites=true&w=majority';
+//const DB_URL = 'mongodb+srv://joseph:fEV5nmsWx1TPd3A2@cluster0-s2bob.mongodb.net/summer-vibes?retryWrites=true&w=majority';
 
-mongoose.connect(DB_URL, {
+const MONGO_URL = process.env.MONGODB_URI || 'mongodb+srv://joseph:fEV5nmsWx1TPd3A2@cluster0-s2bob.mongodb.net/summer-vibes?retryWrites=true&w=majority';
+
+mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true,
